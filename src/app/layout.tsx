e00d78650af55e53813f0d0f431b1c21a470ae73
ahmed ksontini <57683675+ahmedksont@users.noreply.github.com>
2026-05-17 18:26:17 +0100

    upadate about

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 // src/app/layout.tsx
 import "./globals.css";
 import { Toaster } from "sonner";
+
+export const metadata = {
+  title: "EduLink",
+  description: "EduLink is a platform that connects students with investors and opportunities to turn their projects into startups.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 export default function RootLayout({
   children,
 }: {
